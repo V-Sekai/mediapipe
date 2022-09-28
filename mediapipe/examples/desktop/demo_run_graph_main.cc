6aa17cd4fa53ac7705756d5570ee2fc525a21ff2
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2022-09-28 08:13:54 -0700

    Add Linux cicd.
    Add holistic tracking.
    
    
    Trial windows builds.
    
    
    Force 1280x720@60fps.
    
    
    Bazel gpu only works on Linux.
    
    
    Revert "Bazel gpu only works on Linux."
    
    This reverts commit e5cc42552b2ba4961956c1b2900020ce0cefc047.
    
    Default to holistic gpu.
    
    
    Use python 3.
    
    
    Update linux holistic cicd.
    
    
    Remove explicit python path.
    Add osc
    
    Co-Authored-By: tommymitch <teamaxe@hotmail.com>
    Correct typo.
    
    
    Revert.
    
    
    Change the osc demo back to cpu.
    
    
    Remove error.
    
    
    Typo.
    
    
    Move readme.
    
    
    Add new readme.
    
    
    Update linux cicd.
    
    
    Only build holistic osc.
    
    
    Don't need conversion of the color space.
    
    
    Fix windows builds
    
    
    Get wrist of each hand.
    
    
    Use explicit paths.
    
    
    Revert "Don't need conversion of the color space."
    
    This reverts commit 3124903e99021d56c328a687f5a15a3378b8a6f9.
    
    Update readme.
    
    
    Swap osc libraries.
    
    
    Force define OSC_HOST_LITTLE_ENDIAN.
    
    
    Define the two OSC_HOST_LITTLE_ENDIAN cases we care about.
    
    
    Save the proper build locations.
    
    
    Use the correct include for endian.
    
    
    Try again to make compile on windows.
    
    
    Links.
    
    
    Build holistic tracking.
    
    
    Add a real example.
    
    
    Use a different video.
    
    
    Updates and add some enumerations.
    
    
    Update README.md
    Copy the entire output folder.
    
    
    Update README.md
    Update README.md
    Do not build the gpu sample.
    
    
    Add media pipe that uses world land marks.
    
    
    Add holistic tracking with iris.
    
    
    Try only calling the exe instead of the full python path.
    
    
    Remove gpu flag.
    
    
    Update README.md
    Update README.md
    Rename linux_builds.yml to linux_builds.yml.disabled
    Rename windows_builds.yml to windows_builds.yml.disabled

--- mediapipe/examples/desktop/demo_run_graph_main.cc
+++ mediapipe/examples/desktop/demo_run_graph_main.cc
@@ -18,19 +18,68 @@
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/formats/classification.pb.h"
 #include "mediapipe/framework/formats/image_frame.h"
 #include "mediapipe/framework/formats/image_frame_opencv.h"
+#include "mediapipe/framework/formats/landmark.pb.h"
 #include "mediapipe/framework/port/file_helpers.h"
 #include "mediapipe/framework/port/opencv_highgui_inc.h"
 #include "mediapipe/framework/port/opencv_imgproc_inc.h"
 #include "mediapipe/framework/port/opencv_video_inc.h"
 #include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status.h"
+#include "mediapipe/osc/OscOutboundPacketStream.h"
+#include "mediapipe/osc/ip/UdpSocket.h"
+#include "mediapipe/Osc/OscSender.h"
 #include "mediapipe/util/resource_util.h"
+
+#define ADDRESS "127.0.0.1"
+#define PORT 39539
+#define OUTPUT_BUFFER_SIZE 1024
 
 constexpr char kInputStream[] = "input_video";
 constexpr char kOutputStream[] = "output_video";
 constexpr char kWindowName[] = "MediaPipe";
+constexpr char kLandmarksStream[] = "pose_world_landmarks";
+constexpr char kLeftHandLandmarksStream[] = "left_hand_landmarks";
+constexpr char kRightHandLandmarksStream[] = "right_hand_landmarks";
+constexpr char kFaceLandmarksStream[] = "face_landmarks_with_iris";
+
+enum class MEDIAPIPE_POSE {
+    MEDIAPIPE_NOSE = 0,
+    MEDIAPIPE_LEFT_EYE_INNER = 1,
+    MEDIAPIPE_LEFT_EYE = 2,
+    MEDIAPIPE_LEFT_EYE_OUTER = 3,
+    MEDIAPIPE_RIGHT_EYE_INNER = 4,
+    MEDIAPIPE_RIGHT_EYE = 5,
+    MEDIAPIPE_RIGHT_EYE_OUTER = 6,
+    MEDIAPIPE_LEFT_EAR = 7,
+    MEDIAPIPE_RIGHT_EAR = 8,
+    MEDIAPIPE_MOUTH_LEFT = 9,
+    MEDIAPIPE_MOUTH_RIGHT = 10,
+    MEDIAPIPE_LEFT_SHOULDER = 11,
+    MEDIAPIPE_RIGHT_SHOULDER = 12,
+    MEDIAPIPE_LEFT_ELBOW = 13,
+    MEDIAPIPE_RIGHT_ELBOW = 14,
+    MEDIAPIPE_LEFT_WRIST = 15,
+    MEDIAPIPE_RIGHT_WRIST = 16,
+    MEDIAPIPE_LEFT_PINKY = 17,
+    MEDIAPIPE_RIGHT_PINKY = 18,
+    MEDIAPIPE_LEFT_INDEX = 19,
+    MEDIAPIPE_RIGHT_INDEX = 20,
+    MEDIAPIPE_LEFT_THUMB = 21,
+    MEDIAPIPE_RIGHT_THUMB = 22,
+    MEDIAPIPE_LEFT_HIP = 23,
+    MEDIAPIPE_RIGHT_HIP = 24,
+    MEDIAPIPE_LEFT_KNEE = 25,
+    MEDIAPIPE_RIGHT_KNEE = 26,
+    MEDIAPIPE_LEFT_ANKLE = 27,
+    MEDIAPIPE_RIGHT_ANKLE = 28,
+    MEDIAPIPE_LEFT_HEEL = 29,
+    MEDIAPIPE_RIGHT_HEEL = 30,
+    MEDIAPIPE_LEFT_FOOT_INDEX = 31,
+    MEDIAPIPE_RIGHT_FOOT_INDEX = 32,
+};
 
 ABSL_FLAG(std::string, calculator_graph_config_file, "",
           "Name of file containing text format CalculatorGraphConfig proto.");
@@ -42,6 +91,8 @@ ABSL_FLAG(std::string, output_video_path, "",
           "If not provided, show result in a window.");
 
 absl::Status RunMPPGraph() {
+  UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
+  char buffer[OUTPUT_BUFFER_SIZE];
   std::string calculator_graph_config_contents;
   MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
       absl::GetFlag(FLAGS_calculator_graph_config_file),
@@ -69,18 +120,49 @@ absl::Status RunMPPGraph() {
   cv::VideoWriter writer;
   const bool save_video = !absl::GetFlag(FLAGS_output_video_path).empty();
   if (!save_video) {
-    cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
-#if (CV_MAJOR_VERSION >= 3) && (CV_MINOR_VERSION >= 2)
-    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    capture.set(cv::CAP_PROP_FPS, 30);
-#endif
+    cv::namedWindow(kWindowName, 1);
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+    capture.set(cv::CAP_PROP_FPS, 60);
   }
 
   LOG(INFO) << "Start running the calculator graph.";
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
                    graph.AddOutputStreamPoller(kOutputStream));
+
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller_landmarks,
+                   graph.AddOutputStreamPoller(kLandmarksStream));
+
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller left_poller_landmarks,
+                   graph.AddOutputStreamPoller(kLeftHandLandmarksStream));
+
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller right_poller_landmarks,
+                   graph.AddOutputStreamPoller(kRightHandLandmarksStream));
+
+  ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller face_poller_landmarks,
+                   graph.AddOutputStreamPoller(kFaceLandmarksStream));
+
   MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+  enum {
+    LOADED_NOT_LOADED = 0,
+    LOADED_LOADED = 1,
+  };
+  enum {
+    CALIBRATION_STATE_UNCALIBRATED = 0,
+    CALIBRATION_STATE_WAITING_FOR_CALIBRATION = 1,
+    CALIBRATION_STATE_CALIBRATING = 2,
+    CALIBRATION_STATE_CALIBRATED = 3,
+  };
+  enum {
+    CALIBRATION_MODE_NORMAL = 0,
+    CALIBRATION_MODE_MR_NORMAL = 1,
+    CALIBRATION_MODE_MR_FLOOR_FIX = 2,
+  };
+  int32_t loaded = LOADED_LOADED;
+  int32_t calibration_state = CALIBRATION_STATE_CALIBRATED;
+  int32_t calibration_mode = CALIBRATION_MODE_NORMAL;
+
 
   LOG(INFO) << "Start grabbing and processing frames.";
   bool grab_frames = true;
@@ -116,11 +198,104 @@ absl::Status RunMPPGraph() {
         kInputStream, mediapipe::Adopt(input_frame.release())
                           .At(mediapipe::Timestamp(frame_timestamp_us))));
 
-    // Get the graph result packet, or stop if that fails.
+    // Get the graph result packet and stop if it fails.
     mediapipe::Packet packet;
-    if (!poller.Next(&packet)) break;
-    auto& output_frame = packet.Get<mediapipe::ImageFrame>();
+    if (!poller.Next(&packet))
+      break;
 
+    osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+      p << osc::BeginBundleImmediate << osc::BeginMessage( "/VMC/Ext/OK" ) << loaded \
+      << calibration_state << calibration_mode << osc::EndMessage << osc::EndBundle;
+    transmitSocket.Send( p.Data(), p.Size() );
+    
+    mediapipe::Packet landmark_packet;
+
+    if (poller_landmarks.QueueSize() &&
+        poller_landmarks.Next(&landmark_packet)) {
+      auto &landmarkList =
+          landmark_packet.Get<::mediapipe::LandmarkList>();
+
+      for (int j = 0; j < landmarkList.landmark_size(); j++) {
+        auto &landmark = landmarkList.landmark(j);
+          osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+          p << osc::BeginBundleImmediate << osc::BeginMessage( "/VMC/Ext/Bone/Pos" ) 
+            << (std::string("pose_") + std::to_string(j)).c_str() << 
+              (float)landmark.x() << (float)landmark.y() << (float)landmark.z() << 
+              (float) 0.0 << (float)0.0  << (float)0.0  << (float)1.0 << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              osc::EndMessage
+        << osc::EndBundle;
+        transmitSocket.Send( p.Data(), p.Size() );
+      }
+    }
+
+    mediapipe::Packet face_landmark_packet;
+
+    if (face_poller_landmarks.QueueSize() &&
+        face_poller_landmarks.Next(&face_landmark_packet)) {
+      auto &landmarkList =
+          face_landmark_packet.Get<::mediapipe::NormalizedLandmarkList>();
+
+      for (int j = 0; j < landmarkList.landmark_size(); j++) {
+        auto &landmark = landmarkList.landmark(j);
+          osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+          p << osc::BeginBundleImmediate
+        << osc::BeginMessage( "/VMC/Ext/Bone/Pos" ) 
+            << (std::string("face_") + std::to_string(j)).c_str() << 
+              (float)landmark.x() << (float)landmark.y() << (float)landmark.z() << 
+              (float) 0.0 << (float)0.0  << (float)0.0  << (float)1.0 << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              osc::EndMessage
+        << osc::EndBundle;
+        transmitSocket.Send( p.Data(), p.Size() );
+      }
+    }
+
+    mediapipe::Packet left_landmark_packet;
+
+    if (left_poller_landmarks.QueueSize() &&
+        left_poller_landmarks.Next(&left_landmark_packet)) {
+      auto &landmarkList =
+          left_landmark_packet.Get<::mediapipe::NormalizedLandmarkList>();
+
+      for (int j = 0; j < landmarkList.landmark_size(); j++) {
+        auto &landmark = landmarkList.landmark(j);
+        osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+        p << osc::BeginBundleImmediate
+        << osc::BeginMessage( "/VMC/Ext/Bone/Pos" ) << (std::string("left_hand_") + std::to_string(j)).c_str() << \
+              (float)landmark.x() << (float)landmark.y() << (float)landmark.z() << \
+              (float) 0.0 << (float)0.0  << (float)0.0  << (float)1.0 << \
+              (float) 1.0 << (float)1.0  << (float)1.0  << \
+              (float) 1.0 << (float)1.0  << (float)1.0  << \
+              osc::EndMessage
+        << osc::EndBundle;
+        transmitSocket.Send( p.Data(), p.Size() );
+      }
+    }
+    mediapipe::Packet right_landmark_packet;
+    if (right_poller_landmarks.QueueSize() &&
+        right_poller_landmarks.Next(&right_landmark_packet)) {
+      auto &landmarkList =
+          right_landmark_packet.Get<::mediapipe::NormalizedLandmarkList>();
+      for (int j = 0; j < landmarkList.landmark_size(); j++) {
+        auto &landmark = landmarkList.landmark(j);
+        osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+        p << osc::BeginBundleImmediate
+        << osc::BeginMessage( "/VMC/Ext/Bone/Pos" ) 
+            << (std::string("right_hand_") + std::to_string(j)).c_str() << 
+              (float)landmark.x() << (float)landmark.y() << (float)landmark.z() << 
+              (float) 0.0 << (float)0.0  << (float)0.0  << (float)1.0 << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              (float) 1.0 << (float)1.0  << (float)1.0  << 
+              osc::EndMessage
+        << osc::EndBundle;
+        transmitSocket.Send( p.Data(), p.Size() );
+      }
+    }
+
+    auto &output_frame = packet.Get<mediapipe::ImageFrame>();
     // Convert back to opencv for display or saving.
     cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
     cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
@@ -128,26 +303,28 @@ absl::Status RunMPPGraph() {
       if (!writer.isOpened()) {
         LOG(INFO) << "Prepare video writer.";
         writer.open(absl::GetFlag(FLAGS_output_video_path),
-                    mediapipe::fourcc('a', 'v', 'c', '1'),  // .mp4
+                    mediapipe::fourcc('a', 'v', 'c', '1'), // .mp4
                     capture.get(cv::CAP_PROP_FPS), output_frame_mat.size());
         RET_CHECK(writer.isOpened());
       }
       writer.write(output_frame_mat);
     } else {
       cv::imshow(kWindowName, output_frame_mat);
-      // Press any key to exit.
+      // Press space key to exit.
       const int pressed_key = cv::waitKey(5);
-      if (pressed_key >= 0 && pressed_key != 255) grab_frames = false;
+      if (pressed_key >= 0 && pressed_key != 255)
+        grab_frames = false;
     }
   }
 
   LOG(INFO) << "Shutting down.";
-  if (writer.isOpened()) writer.release();
+  if (writer.isOpened())
+    writer.release();
   MP_RETURN_IF_ERROR(graph.CloseInputStream(kInputStream));
   return graph.WaitUntilDone();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   absl::ParseCommandLine(argc, argv);
   absl::Status run_status = RunMPPGraph();
